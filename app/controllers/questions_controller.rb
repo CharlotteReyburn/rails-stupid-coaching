9e9ab6b3c9b8311424783ef2d9bb72a0f1796120
Charlotte Reyburn <charlottereyburn7@gmail.com>
2023-08-21 13:41:56 +0100

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    user_question = params[:question]
+    @answer = generate_answer(user_question)
+  end
+
+  private
+  def generate_answer(question)
+    if question == "I am going to work"
+      "Great!"
+    elsif question.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
